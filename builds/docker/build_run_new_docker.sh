919f0d150e9978f673e4b73ee358f7aa6f9251c3
osamsam <osamsam321@gmail.com>
2024-09-28 20:14:58 -0500

    made some changes

--- builds/docker/build_run_new_docker.sh
+++ builds/docker/build_run_new_docker.sh
@@ -1,33 +1,51 @@
 #!/bin/sh
+
 if [ $# -eq 0 ]; then
     echo "no argument were provided. Please provide the .env.list file to pass in. It should be list as .env.list.prod or .env.list.dev"
     exit 1;
 fi
+
 echo "Your file is $1"
-echo "Your tag is $2"
+
 if [ "$1" = ".env.file.prod" ] || [ "$1" = ".env.file.dev" ]; then
+
+
+    #INIT
+
+    docker_build_dir=`pwd`
     env_file=`realpath $1`
+        docker_tag='stage_player_docker_v1'
+
+    #STOP
+
     echo "Going to stop old docker. This make take some time..."
-    docker_tag='stage_player_docker_v1'
-    username='bigdady1235'
-    docker_remote='bigdady1235/stage_player'
-    docker stop `docker ps -q`
-    #place your own app build here ...
-    ../app/build_stage_player.sh
+    container_id=$(docker ps -aqf "name=$docker_tag")
+    if [ -n "$container_id" ]; then
+        docker stop "$container_id"
+        docker rm "$container_id"
+    else
+        echo "Container not found."
+    fi
+
+    #BUILD
+
     echo "running your app builds"
     echo "Going to build and run the new docker container..."
     echo "changing working directory to execute dockerfile"
+    ../app/build_stage_player.sh
     cd ../../
     docker build  -t "$docker_tag" .
-    docker tag "$docker_tag"  "$docker_remote:"$docker_tag
-    docker run --env-file "$env_file" -p 3000:3000 -p 80:80 -d "$docker_tag"
-    echo "now pushing to docker hub with your criteria"
-    docker push "$docker_remote:$docker_tag"
-    #docker exec -it `docker ps -q` /bin/sh
+
+    #RUN
+
+    docker_container_id=$(docker run --name $docker_tag --env-file "$env_file" -p 3000:3000 -p 80:80 -d "$docker_tag")
+    echo "your container id: $docker_container_id"
     echo "done"
-    else
-        echo "Incorrect parameter. Please provide the .env.list file to pass in. It should be .env.file.prod or .env.file.dev"
-        exit 1;
+
+
+else
+    echo "Incorrect parameter. Please provide the .env.list file to pass in. It should be .env.file.prod or .env.file.dev"
+    exit 1;
 fi
 
 
