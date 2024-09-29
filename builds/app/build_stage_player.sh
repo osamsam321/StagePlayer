215bc9efafff02643127f7acfe61489916fb205a
Odisho <47095205+osamsam321@users.noreply.github.com>
2024-09-29 00:19:36 -0500

    Update build_stage_player.sh

--- builds/app/build_stage_player.sh
+++ builds/app/build_stage_player.sh
@@ -1,11 +1,11 @@
-
 echo "starting build for the stage_player backened";
 cd ../../stage_player_back;
+npm install
 npx tsc;
 wait;
 echo "done";
 echo "starting react build";
 cd ../stage_player_react;
+npm install
 npm run build --production;
 echo "done";
-
